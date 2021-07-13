0c214c8789c1a84ad5e17ad6b12b988bf8f53a0f
missionwansoo <69702443+missionwansoo@users.noreply.github.com>
2021-07-13 11:04:00 +0900

    Add files via upload

--- aboutme/js/self.js
+++ aboutme/js/self.js
@@ -1,21 +1,10 @@
 $(document).ready(function() {
 		
   $('.container2').waypoint(function() {$('.chart ul li p.chart_line .line').addClass('fadeInLeft');}, {offset: '50%'});
+	
+  $('.con4_1').waypoint(function() {$('.con4_1').addClass('fadeInLeft');}, {offset: '80%'});
+  $('.con4_2').waypoint(function() {$('.con4_2').addClass('fadeInLeft');}, {offset: '80%'});
+  $('.con4_3').waypoint(function() {$('.con4_3').addClass('fadeInLeft');}, {offset: '80%'});
+  $('.con4_4').waypoint(function() {$('.con4_4').addClass('fadeInLeft');}, {offset: '80%'});
     
-  $('.thinking').css('opacity', 0);
-  $('.time1').css('opacity', 0);
-  $('.time2').css('opacity', 0);
-  $('.time3').css('opacity', 0);
-  $('.time4').css('opacity', 0);
-  $('.time5').css('opacity', 0);
-  $('.time6').css('opacity', 0);
-    
-  $('.container5').waypoint(function() {$('.thinking').addClass('fadeIn');}, {offset: '100%'});
-  $('.container5').waypoint(function() {$('.timeline_line').addClass('fadeInDown');}, {offset: '80%'});
-  $('.time1').waypoint(function() {$('.time1').addClass('fadeInLeft');}, {offset: '80%'});
-  $('.time2').waypoint(function() {$('.time2').addClass('fadeInLeft');}, {offset: '80%'});
-  $('.time3').waypoint(function() {$('.time3').addClass('fadeInLeft');}, {offset: '80%'});
-  $('.time4').waypoint(function() {$('.time4').addClass('fadeInLeft');}, {offset: '80%'});
-  $('.time5').waypoint(function() {$('.time5').addClass('fadeInLeft');}, {offset: '80%'});
-  $('.time5').waypoint(function() {$('.time6').addClass('fadeInLeft');}, {offset: '80%'});
 });
